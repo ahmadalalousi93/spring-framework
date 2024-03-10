0991989bddbf08e06796e1eebd3b8c132554e84d
ahmadalalousi <ahmad.o.alalousi@gmail.com>
2024-03-09 19:40:28 -0500

    Spring MVC Demo and Grouping

--- Spring-07-Spring-MVC-Controller/src/main/java/com/cydeo/controller/HomeController.java
+++ Spring-07-Spring-MVC-Controller/src/main/java/com/cydeo/controller/HomeController.java
@@ -3,15 +3,20 @@ package com.cydeo.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-@Controller
+@Controller  //Anotate the class with @Controller stereotype annotation
 public class HomeController {
-    @RequestMapping("/home")
+    @RequestMapping("/home") //use @RequestMapping annotation to associate the action with an HTTP request path
     public String home(){
         return "home.html";
-    }
+    } //return the hrml document name that ocntains the details we want the broswer to display
 
     @RequestMapping("/welcome")
     public String home2(){
+        return "welcome.html";
+    }
+
+    @RequestMapping //this always goes to default and makes this or ":/" the default
+    public String home3(){
         return "welcome.html";
     }
 
